839271e925ee50507a28bbbb27a543ca3059ec38
Pathum Liyanagama <pathumpml.dev@gmail.com>
2019-10-31 09:07:45 +0530

    inserting an item

--- app.js
+++ app.js
@@ -1,5 +1,49 @@
 var budgetController = (function () {
+    var Expense = function (id,description, value) {
+        this.id = id;
+        this.description = description;
+        this.value = value;
+        this.type = "exp";
+    }
 
+    var Income = function (id, description, value) {
+        this.id = id;
+        this.description = description;
+        this.value = value;
+        this.type = "inc";
+    }
+
+    var data = {
+        allItems: {
+            exp: [],
+            inc: [],
+            expID: 0,
+            incID: 0
+        },
+        totals: {
+            exp: 0,
+            inc: 0
+        }
+    }
+
+    
+
+    return {
+        addItem: function (type, desc, val) {
+            var newItem;
+            if (type === "exp") {
+                newItem = new Expense(data.allItems.expID, desc, val)              
+            } else if (type === 'inc') {
+                newItem = new Income(data.allItems.incID, desc, val)
+            }
+            data.allItems[type].push(newItem);
+            data.allItems[type+"ID"] += 1;
+            data.totals[type] += val;
+
+            return newItem;
+        }
+
+    }
 
 
 })();
@@ -10,7 +54,9 @@ var UIController = (function () {
         inputType: ".add__type",
         inputDescription: ".add__description",
         inputValue: ".add__value",
-        btnAdd: ".add__btn"
+        btnAdd: ".add__btn",
+        incomeList: ".income__list",
+        expensesList: ".expenses__list"
     }
 
     return {
@@ -24,6 +70,51 @@ var UIController = (function () {
 
         getDOMstrings: function () {
             return DOMstrings
+        },
+
+        addItemToUI: function(item){
+            var html, listClass;
+
+            if(item.type === 'exp'){
+                html = `
+                <div class="item clearfix" id="expense-%id%">
+                    <div class="item__description">%desc%</div>
+                    <div class="right clearfix">
+                        <div class="item__value">- %val%</div>
+                        <div class="item__percentage">21%</div>
+                        <div class="item__delete">
+                            <button class="item__delete--btn"><i class="ion-ios-close-outline"></i></button>
+                        </div>
+                    </div>
+                </div>`;
+                
+                html = html.replace('%id%', item.id);
+                html = html.replace('%desc%', item.description);
+                html = html.replace('%val%', item.value);
+
+                listClassDOM = document.querySelector(DOMstrings.expensesList);
+                listClassDOM.insertAdjacentHTML('beforeend', html);
+
+            }
+            else{
+                html = `
+                <div class="item clearfix" id="income-%id%">
+                    <div class="item__description">%desc%</div>
+                    <div class="right clearfix">
+                        <div class="item__value">+ %val%</div>
+                            <div class="item__delete">
+                            <button class="item__delete--btn"><i class="ion-ios-close-outline"></i></button>
+                        </div>
+                     </div>
+                </div>`
+
+                html = html.replace('%id%', item.id);
+                html = html.replace('%desc%', item.description);
+                html = html.replace('%val%', item.value);
+                
+                listClassDOM = document.querySelector(DOMstrings.incomeList);
+                listClassDOM.insertAdjacentHTML('beforeend', html);
+            }
         }
     }
 
@@ -47,6 +138,8 @@ var appController = (function (budgetCtrlr, UIContrlr) {
     var ctrlAddItem = function () {
         var inputs = UIContrlr.getInput();
         console.log(inputs);
+        var newItem = budgetCtrlr.addItem(inputs.type, inputs.description, inputs.value);
+        UIContrlr.addItemToUI(newItem);
     }
 
     return {
